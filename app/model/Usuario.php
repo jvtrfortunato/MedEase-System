4d57cc4c8dfffdfdb477afb6741754345f5504fa
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-05-05 17:30:57 -0300

    🛠️ Implementação dos métodos getters e setters na classe Usuario

--- app/model/Usuario.php
+++ app/model/Usuario.php
@@ -16,55 +16,85 @@ Class Usuario {
         private string $email,
         private string $senha,
         ) {
-        }
+    }
 
-    public function getId(): int {
-        return $this->id;
+    // Getters
+    public function getIdUsuario(): int {
+        return $this->idUsuario;
     }
 
     public function getNome(): string {
         return $this->nome;
     }
 
-    public function setNome($nome): void {
-        $this->nome = $nome;
-    }
-
     public function getCpf(): string {
         return $this->cpf;
     }
-
-    public function setCpf($cpf): void {
-        $this->cpf = $cpf;
-    }
-
-    public function getTelefone() {
+    
+    public function getTelefone(): string {
         return $this->telefone;
     }
 
-    public function setTelefone($telefone): void {
-        $this->telefone = $telefone;
+    public function getDataNascimento(): string {
+        return $this->dataNascimento;
     }
 
-    public function getEmail() {
+    public function getSexo(): string {
+        return $this->sexo;
+    }
+
+    public function getEmail(): string {
         return $this->email;
     }
 
-    public function setEmail($email): void {
-        $this->email = $email;
+    public function getSenha(): string {
+        return $this->senha;
     }
 
     public function getTipo(): string {
         return $this->tipo;
     }
 
+    public function getEndereco(): Endereco {
+        return $this->endereco;
+    }
+    
+    // Setters
+    public function setNome($nome): void {
+        $this->nome = $nome;
+    }
+
+    public function setCpf($cpf): void {
+        $this->cpf = $cpf;
+    }  
+
+    public function setTelefone($telefone): void {
+        $this->telefone = $telefone;
+    } 
+
+    public function setDataNascimento($dataNascimento): void {
+        $this->dataNascimento = $dataNascimento;
+    } 
+
+    public function setSexo($sexo): void {
+        $this->sexo = $sexo;
+    } 
+
+    public function setEmail($email): void {
+        $this->email = $email;
+    }
+
+    public function setSenha($senha): void {
+        $this->senha = $senha;
+    } 
+
     public function setTipo(): void {
         $this->tipo = $tipo;
     }
 
-    public function verificarSenha($senha) {
-        return password_verify($senha, $this->senha);
-    }
+    public function setEndereco($endereco): void {
+        $this->endereco = $endereco;
+    } 
 }
 
 //$usuario = new Usuario(1,'a','1','1','1','1','1');
